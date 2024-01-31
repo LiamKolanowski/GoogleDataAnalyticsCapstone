cc670c9aedad6dd8999e7e0874ebb134d788c373
LiamKolanowski <liamkolanowski@gmail.com>
2024-01-31 16:01:36 -0700

    Delete tables
    Now redundant, the individual tables are removed from the current workspace

---
+++ delete_tables.sql
@@ -0,0 +1,13 @@
+/*  Delete individual tables from local database  */
+DROP TABLE "202212-divvy-tripdata";
+DROP TABLE "202301-divvy-tripdata";
+DROP TABLE "202302-divvy-tripdata";
+DROP TABLE "202303-divvy-tripdata";
+DROP TABLE "202304-divvy-tripdata";
+DROP TABLE "202305-divvy-tripdata";
+DROP TABLE "202306-divvy-tripdata";
+DROP TABLE "202307-divvy-tripdata";
+DROP TABLE "202308-divvy-tripdata";
+DROP TABLE "202309-divvy-tripdata";
+DROP TABLE "202310-divvy-tripdata";
+DROP TABLE "202311-divvy-tripdata";
