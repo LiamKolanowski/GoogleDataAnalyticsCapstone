efc6bcd3d12e79bd380d9818e8da3902e5f4f878
LiamKolanowski <liamkolanowski@gmail.com>
2024-01-31 09:39:54 -0700

    Combine tables
    This consolidation simplifies data management by centralizing information into one table

---
+++ combine_tables.sql
@@ -0,0 +1,16 @@
+/*  Create a new table with the same structure as "202212-divvy-tripdata"*/
+CREATE TABLE IF NOT EXISTS combined_table AS SELECT * FROM "202212-divvy-tripdata" WHERE 0;
+
+/*  Insert data into the new table*/
+INSERT INTO combined_table SELECT * FROM "202212-divvy-tripdata";
+INSERT INTO combined_table SELECT * FROM "202301-divvy-tripdata";
+INSERT INTO combined_table SELECT * FROM "202302-divvy-tripdata";
+INSERT INTO combined_table SELECT * FROM "202303-divvy-tripdata";
+INSERT INTO combined_table SELECT * FROM "202304-divvy-tripdata";
+INSERT INTO combined_table SELECT * FROM "202305-divvy-tripdata";
+INSERT INTO combined_table SELECT * FROM "202306-divvy-tripdata";
+INSERT INTO combined_table SELECT * FROM "202307-divvy-tripdata";
+INSERT INTO combined_table SELECT * FROM "202308-divvy-tripdata";
+INSERT INTO combined_table SELECT * FROM "202309-divvy-tripdata";
+INSERT INTO combined_table SELECT * FROM "202310-divvy-tripdata";
+INSERT INTO combined_table SELECT * FROM "202311-divvy-tripdata";
