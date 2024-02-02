4d66caf9461420a97244a2762ea7a9c90f2cf410
LiamKolanowski <liamkolanowski@gmail.com>
2024-02-02 13:26:48 -0700

    Fill Season column
    Gives the an identifying string to determine the season

---
+++ fill_season_column.sql
@@ -0,0 +1,9 @@
+/*  Fill season column with identifying strings  */
+UPDATE cyclistic_data
+SET Season = 
+    CASE 
+        WHEN strftime('%m', Started_Datetime) IN ('12', '01', '02') THEN 'Winter'
+        WHEN strftime('%m', Started_Datetime) IN ('03', '04', '05') THEN 'Spring'
+        WHEN strftime('%m', Started_Datetime) IN ('06', '07', '08') THEN 'Summer'
+        WHEN strftime('%m', Started_Datetime) IN ('09', '10', '11') THEN 'Fall'
+    END;
