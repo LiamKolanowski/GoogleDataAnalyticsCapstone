bc8e157bfdb590519ae7ef7583899fe71cc48665
LiamKolanowski <liamkolanowski@gmail.com>
2024-02-02 14:01:18 -0700

    Fill Day_Of_Week column
    Populates the Day_Of_Week column with an identifying string

---
+++ fill_day_of_week_column.sql
@@ -0,0 +1,12 @@
+/*  Fill Day_Of_Week column with an identifying string  */
+UPDATE cyclistic_data
+SET Day_Of_Week = 
+    CASE 
+        WHEN strftime('%w', Started_Datetime) = '0' THEN 'Sunday'
+        WHEN strftime('%w', Started_Datetime) = '1' THEN 'Monday'
+        WHEN strftime('%w', Started_Datetime) = '2' THEN 'Tuesday'
+        WHEN strftime('%w', Started_Datetime) = '3' THEN 'Wednesday'
+        WHEN strftime('%w', Started_Datetime) = '4' THEN 'Thursday'
+        WHEN strftime('%w', Started_Datetime) = '5' THEN 'Friday'
+        WHEN strftime('%w', Started_Datetime) = '6' THEN 'Saturday'
+    END;
