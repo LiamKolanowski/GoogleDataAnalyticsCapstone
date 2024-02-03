d8109d56bd739766022a268de165bc7ace23e313
LiamKolanowski <liamkolanowski@gmail.com>
2024-02-03 12:54:47 -0700

    Remove the negative duration rows
    Deletes the rows that contain rides that are recorded as having started at a time later than they ended, which is not possible

--- remove_impossible_rides.sql
+++ remove_impossible_rides.sql
@@ -1,4 +1,4 @@
-/*  Remove rows where the duration of bike rides
+/*  Remove the 195 rows where the duration of bike rides
   is recorded as negative seconds, which is not possible   */
 DELETE FROM cyclistic_data
 WHERE Ride_Length_In_Seconds < 0;
